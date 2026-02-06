ec80806452d64f1c99025d611749ebb8eee98cff
bharat <bharatchoutharyb@gmail.com>
2026-02-06 18:13:27 +0530

    done

--- class-9/App.js
+++ class-9/App.js
@@ -1,4 +1,4 @@
-// Write a program that takes an integer n as input and computes the sum of its digits. . The input number is always positive.
+// Question :Write a program that takes an integer n as input and computes the sum of its digits. . The input number is always positive.
 
 // function sumOfDigits(n) {
 //     let sum = 0;
@@ -11,7 +11,7 @@
 // 
 // module.exports = { sumOfDigits };
 
-// A number is called an Automorphic number if its square ends with the number itself. For example: . 25 is Automorphic because: 25^2=625 . The last two digits are 25 (which matches the number itself). Write a program that accepts a positive integer n and checks whether it is an Automorphic number. · If n is an Automorphic number, print "Yes". Otherwise, print "No".
+// Question :A number is called an Automorphic number if its square ends with the number itself. For example: . 25 is Automorphic because: 25^2=625 . The last two digits are 25 (which matches the number itself). Write a program that accepts a positive integer n and checks whether it is an Automorphic number. · If n is an Automorphic number, print "Yes". Otherwise, print "No".
 
 // function isAutomorphic(n) {
 //    let square = n * n;
