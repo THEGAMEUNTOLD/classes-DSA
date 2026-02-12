50d24994c416f40aded89bad60af435a62100ea9
bharat <bharatchoutharyb@gmail.com>
2026-02-12 20:36:17 +0530

    done

---
+++ class-32/App.js
@@ -0,0 +1,84 @@
+// You are given an array of integers. Your task is to find and print the frequency of each element in the array in the order of their first appearance. Each output line should contain the element followed by its frequency, separated by a space.
+
+// class Solution {
+//     findFrequencies(arr) {
+//         const freq = new Map();
+// 
+//         for (const num of arr) {
+//             freq.set(num, (freq.get(num) || 0) + 1);
+//         }
+// 
+//         const seen = new Set();
+//         const result = [];
+// 
+//         for (const num of arr) {
+//             if (!seen.has(num)) {
+//                 result.push(num + " " + freq.get(num));
+//                 seen.add(num);
+//             }
+//         }
+// 
+//         return result.join("\n");
+//     }
+// }
+// 
+// module.exports = Solution;
+
+// You are given an array of integers. Your task is to find the sum of all elements that appear exactly once in the array. For example, in the array [1, 2, 3, 2], only 1 and 3 appear once. So, the output will be 1+ 3 = 4.
+
+// class Solution {
+//     sumOfUnique(nums) {
+//         const freq = new Map();
+// 
+//         // count frequency
+//         for (const ch of nums) {
+//             freq.set(ch, (freq.get(ch) || 0) + 1);
+//         }
+// 
+//         let sum = 0;
+// 
+//         // sum elements that appear once
+//         for (const [num, count] of freq) {
+//             if (count === 1) {
+//                 sum += Number(num);
+//             }
+//         }
+// 
+//         return sum;
+//     }
+// }
+// 
+// module.exports = Solution;
+
+// Given an array of integers, find the most frequent even element. If there are multiple even elements with the same highest frequency, return the smallest one. If there are no even elements, return-1.
+
+// class Solution {
+//     mostFrequentEven(nums) {
+//         const freq = new Map();
+// 
+//         // count only even numbers
+//         for (const ch of nums) {
+//             const num = Number(ch);
+//             if (num % 2 === 0) {
+//                 freq.set(num, (freq.get(num) || 0) + 1);
+//             }
+//         }
+// 
+//         if (freq.size === 0) return -1;
+// 
+//         let bestNum = -1;
+//  
+//         let maxFreq = 0;
+// 
+//         for (const [num, count] of freq) {
+//             if (count > maxFreq || (count === maxFreq && num < bestNum)) {
+//                 maxFreq = count;
+//                 bestNum = num;
+//             }
+//         }
+// 
+//         return bestNum;
+//     }
+// }
+// 
+// module.exports = Solution;
---
+++ class-33/App.js
@@ -0,0 +1,65 @@
+// You are given a string s consisting of lowercase English letters. Return the first letter that appears twice in the string when scanned from left to right.
+
+// /**
+//  * Find the first character that appears twice in a string.
+//  * @param {string} s - The input string
+//  * @return {string} - The first repeated character
+//  */
+// function repeatedCharacter(s) {
+//     const seen = new Set();
+
+//     for (const ch of s) {
+//         if (seen.has(ch)) {
+//             return ch;
+//         }
+//         seen.add(ch);
+//     }
+
+//     return ""; // in case no character repeats
+// }
+
+// module.exports = { repeatedCharacter };
+
+// Given an array of integers nums and an integer target, your task is to find two distinct indices in the array such that the numbers at these indices add up to target. You are guaranteed that there is exactly one solution, and you cannot use the same element twice.
+
+// /**
+//  * Find the two indices in the array such that their sum equals the target.
+//  * @param {number[]} nums - The array of numbers.
+//  * @param {number} target - The target sum.
+//  * @return {number[]} - An array containing the two indices.
+//  */
+// function twoSum(nums, target) {
+//     const map = new Map(); // number -> index
+// 
+//     for (let i = 0; i < nums.length; i++) {
+//         const complement = target - nums[i];
+// 
+//         if (map.has(complement)) {
+//             return [map.get(complement), i];
+//         }
+// 
+//         map.set(nums[i], i);
+//     }
+// }
+// 
+// module.exports = { twoSum };
+
+// You are given two arrays: . names, where names[i] is the name of the i-th person. . heights, where heights[i] is the height of the i-th person. Return the array of names sorted in descending order by height.
+
+// /**
+//  * Sort people by height in descending order.
+//  * @param {string[]} names - Array of names
+//  * @param {number[]} heights - Array of heights
+//  * @return {string[]} - Sorted names by descending height
+//  */
+// function sortPeople(names, heights) {
+//     return names
+//         .map((name, i) => [name, heights[i]])   // pair name with height
+//         .sort((a, b) => b[1] - a[1])            // sort by height descending
+//         .map(pair => pair[0]);                  // extract sorted names
+// }
+
+// module.exports = { sortPeople };
+
+
+// 
