1f39c6cb03d44ec033cbf3fc7d95439b0f533f7b
bharat <bharatchoutharyb@gmail.com>
2026-02-13 15:10:25 +0530

    done

---
+++ class-36/App.js
@@ -0,0 +1,28 @@
+// Write a program that accepts two integers and swaps their values without using a third variable. After swapping, return the new values of the two numbers.
+
+// function swapNumbers(a, b) {
+//     a = a + b;
+//     b = a - b;
+//     a = a - b;
+//     return [a, b];
+// }
+
+// module.exports = { swapNumbers };
+
+// Write a program that takes an integer as input and checks whether the number is even or odd. . If the number is even, return "Even". . If the number is odd, return "Odd".
+
+// function checkEvenOrOdd(n) {
+//     return n % 2 === 0 ? "Even" : "Odd";
+// }
+
+// module.exports = { checkEvenOrOdd };
+
+// Given an integer n, your task is to return true if n is a power of two, otherwise return false. A number n is a power of two if it can be written as 2^x where x is a non-negative integer.
+
+// function isPowerOfTwo(n) {
+//     if (n <= 0) return false;
+//     return (n & (n - 1)) === 0;
+// }
+
+// module.exports = { isPowerOfTwo };
+
